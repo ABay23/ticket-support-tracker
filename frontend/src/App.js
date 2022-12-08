07529d236601ad31077a17ae79f457f499ecf1a0
Alejandro Bay <alejandrobay23@gmail.com>
2022-12-08 01:38:23 -0500

    Register Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 import Header from './components/Header'
 import Home from './pages/Home'
 import Login from './pages/Login'
