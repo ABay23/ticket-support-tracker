6534bd6e95f85223eb60ae6bf11ffff34e5be0aa
Alejandro Bay <alejandrobay23@gmail.com>
2023-01-02 21:05:22 -0500

    New Register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,12 +1,7 @@
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify'
 import { FaUser } from 'react-icons/fa'
-import { useSelector, useDispatch } from 'react-redux'
-// import { register } from '../features/auth/authSlice'
-// import Spinner from '../components/Spinner'
 
-function Register() {
+const Register = () => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -16,11 +11,6 @@ function Register() {
 
   const { name, email, password, password2 } = formData
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-
-  // const { isLoading } = useSelector((state) => state.auth)
-
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -28,52 +18,17 @@ function Register() {
     }))
   }
 
-  // NOTE: no need for useEffect here as we can catch the
-  // AsyncThunkAction rejection in our onSubmit or redirect them on the
-  // resolution
-  // Side effects shoulld go in event handlers where possible
-  // source: - https://beta.reactjs.org/learn/keeping-components-pure#where-you-can-cause-side-effects
-
-  const onSubmit = (e) => {
-    e.preventDefault()
-
-    if (password !== password2) {
-      toast.error('Passwords do not match')
-    } else {
-      const userData = {
-        name,
-        email,
-        password,
-      }
-
-      // dispatch(register(userData))
-      //   .unwrap()
-      //   .then((user) => {
-      //     // NOTE: by unwrapping the AsyncThunkAction we can navigate the user after
-      //     // getting a good response from our API or catch the AsyncThunkAction
-      //     // rejection to show an error message
-      //     toast.success(`Registered new user - ${user.name}`)
-      //     navigate('/')
-      //   })
-      //   .catch(toast.error)
-    }
-  }
-
-  // if (isLoading) {
-  //   return <Spinner />
-  // }
-
   return (
     <>
       <section className='heading'>
         <h1>
           <FaUser /> Register
         </h1>
-        <p>Please create an account</p>
+        <p>Please Create an account</p>
       </section>
 
       <section className='form'>
-        <form onSubmit={onSubmit}>
+        <form>
           <div className='form-group'>
             <input
               type='text'
@@ -83,47 +38,7 @@ function Register() {
               value={name}
               onChange={onChange}
               placeholder='Enter your name'
-              required
             />
-          </div>
-          <div className='form-group'>
-            <input
-              type='email'
-              className='form-control'
-              id='email'
-              name='email'
-              value={email}
-              onChange={onChange}
-              placeholder='Enter your email'
-              required
-            />
-          </div>
-          <div className='form-group'>
-            <input
-              type='password'
-              className='form-control'
-              id='password'
-              name='password'
-              value={password}
-              onChange={onChange}
-              placeholder='Enter password'
-              required
-            />
-          </div>
-          <div className='form-group'>
-            <input
-              type='password'
-              className='form-control'
-              id='password2'
-              name='password2'
-              value={password2}
-              onChange={onChange}
-              placeholder='Confirm password'
-              required
-            />
-          </div>
-          <div className='form-group'>
-            <button className='btn btn-block'>Submit</button>
           </div>
         </form>
       </section>
