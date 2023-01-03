2438c92560fe9236dee9ba20e6abfc2596a19a7e
Alejandro Bay <alejandrobay23@gmail.com>
2023-01-03 00:41:32 -0500

    Login And Redux

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
-  return <h1>Home</h1>
+  return (
+    <>
+      <section className='heading'>
+        <h1>What do you need help width?</h1>
+        <p>Please choose from an option below</p>
+      </section>
+
+      <Link to='/new-ticket' className='btn btn-reverse btn-block'>
+        <FaQuestionCircle /> Create New Ticket
+      </Link>
+      <Link to='/tickets' className='btn btn-block'>
+        <FaTicketAlt /> View my Ticket
+      </Link>
+    </>
+  )
 }
 
 export default Home
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import { FaUser } from 'react-icons/fa'
+import { toast } from 'react-toastify'
+import { useSelector, useDispatch } from 'react-redux'
+import { register } from '../features/auth/authSlice'
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -11,11 +14,32 @@ const Register = () => {
 
   const { name, email, password, password2 } = formData
 
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if (password !== password2) {
+      toast.error('passwords do not match!')
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      }
+      dispatch(register(userData))
+    }
   }
 
   return (
@@ -28,7 +52,7 @@ const Register = () => {
       </section>
 
       <section className='form'>
-        <form>
+        <form onSubmit={onSubmit}>
           <div className='form-group'>
             <input
               type='text'
@@ -38,7 +62,47 @@ const Register = () => {
               value={name}
               onChange={onChange}
               placeholder='Enter your name'
+              required
             />
+          </div>
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              onChange={onChange}
+              placeholder='Enter your email'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              onChange={onChange}
+              placeholder='Enter your password'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password2'
+              name='password2'
+              value={password2}
+              onChange={onChange}
+              placeholder='Confirm your password'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-block'>Submit</button>
           </div>
         </form>
       </section>
