c92cad4cf33e7b79a1448032e8d61ce388eb1625
Alejandro Bay <alejandrobay23@gmail.com>
2023-01-04 23:46:04 -0500

    Add Tickets

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const { getTickets, createTickets } = require('../controllers/ticketController')
+const {
+  getTickets,
+  getTicket,
+  createTickets,
+} = require('../controllers/ticketController')
 
 const { protect } = require('../middleware/authMiddleware')
 
 router.route('/').get(protect, getTickets).post(protect, createTickets)
+
+router.route('/:id').get(protect, getTicket)
 
 module.exports = router
