8fe05d6d1dbce2c9537da0be62a240e02b59cd57
Alejandro Bay <alejandrobay23@gmail.com>
2023-01-07 19:36:31 -0500

    Token err

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -25,7 +25,7 @@ const protect = asyncHandler(async (req, res, next) => {
       // https://www.udemy.com/course/react-front-to-back-2022/learn/lecture/30591026#questions/17843570
       if (!req.user) {
         res.status(401)
-        throw new Error('Not authorised')
+        throw new Error('Not authorized')
       }
 
       next()
